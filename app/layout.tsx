c0513a9deb679507f89636e40c00ab1978421700
Santosh Kumar <santoshkumar15novmth@gmail.com>
2024-03-31 00:10:32 +0530

    implement theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,50 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
-import React from 'react';
-import {Inter, Space_Grotesk} from 'next/font/google';
-import type { Metadata } from 'next';
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
 const inter = Inter({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-inter'
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
 });
 
 const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-spaceGrotesk'
-})
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export const metadata: Metadata = {
-  title: 'Stack Overflow',
-  description: 'A community-driven platform for asking ans answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app developement, algorithms, data structures, and more.',
+  title: "Stack Overflow",
+  description:
+    "A community-driven platform for asking ans answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app developement, algorithms, data structures, and more.",
   icons: {
-    icon: '/assets/images/site-logo.svg'
-  }
-}
- 
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-    appearance={{
-      elements: {
-        formButtonPrimary: 'primary-gradient',
-        footerActionLink: 'primary-text-gradient hover:text-primary-500'
-      }
-    }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
-  )
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+        <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
 }
