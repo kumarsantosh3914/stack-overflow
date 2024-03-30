6fafab67e3d0259bfb66a5db2a4cc111f69f41e6
Santosh Kumar <santoshkumar15novmth@gmail.com>
2024-03-30 16:54:06 +0530

    vercel testing

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="h1-bold">Next.js 13 we&apos;re coming!</h1>
+      <h1 className="h1-bold">Next.js 13 we&apos;re coming! something</h1>
     </div>
   );
 }
