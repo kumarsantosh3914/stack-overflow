701ca8f0f61c0124e8d0a10b85bbc2aab894fd7a
Santosh Kumar <santoshkumar15novmth@gmail.com>
2024-04-01 20:00:31 +0530

    fetch questions done

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,45 +6,13 @@ import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 import NoResult from "@/components/shared/NoResult";
 import QuestionCard from "@/components/cards/QuestionCard";
+import { getQuestions } from "@/lib/actions/question.action";
 
-const questions = [
-  {
-    _id: "1",
-    title: "Cascading Deletes in SQLAlchemy?",
-    tags: [
-      { _id: "1", name: "python" },
-      { _id: "2", name: "sql" },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      picture: "",
-    },
-    upvotes: 10,
-    views: 100,
-    answers: [],
-    createdAt: new Date("2021-09-01T12:00:00.000Z"),
-  },
-  {
-    _id: "2",
-    title: "Cascading Deletes in SQLAlchemy?",
-    tags: [
-      { _id: "1", name: "python" },
-      { _id: "2", name: "sql" },
-    ],
-    author: {
-      _id: "2",
-      name: "John Doe",
-      picture: "",
-    },
-    upvotes: 10,
-    views: 100,
-    answers: [],
-    createdAt: new Date("2021-09-01T12:00:00.000Z"),
-  },
-];
 
-export default function Home() {
+export default async function Home() {
+  const result = await getQuestions({});
+  // console.log(result);
+
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -75,8 +43,8 @@ export default function Home() {
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {questions.length > 0 ? (
-          questions.map((question) => (
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -55,7 +55,8 @@ const Question = ({mongoUserId}: Props) => {
              title: values.title,
              content: values.explanation,
              tags: values.tags,
-             author: JSON.parse(mongoUserId)
+             author: JSON.parse(mongoUserId),
+             path: pathname
           });
 
           // navigate to home page
