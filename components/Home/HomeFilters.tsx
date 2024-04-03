7abad5dc134abb1e01f24fcb633e254b54d771cd
Santosh Kumar <santoshkumar15novmth@gmail.com>
2024-04-03 16:25:48 +0530

    edit profile and filtering done

--- components/Home/HomeFilters.tsx
+++ components/Home/HomeFilters.tsx
@@ -1,11 +1,42 @@
 'use client';
 
 import { HomePageFilters } from '@/constants/filters';
-import React from 'react'
 import { Button } from '../ui/button';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { formUrlQuery } from '@/lib/utils';
+
 
 const HomeFilters = () => {
-  const active = 'frequent';
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const [active, setActive] = useState('');
+
+  const handleTypeClick = (item: string) => {
+      if(active === item) {
+        setActive("");
+
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: 'filter',
+          value: null
+        })
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        setActive(item);
+
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: 'filter',
+          value: item.toLowerCase()
+        })
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+
   return (
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
       {HomePageFilters.map((item) => (
@@ -14,6 +45,7 @@ const HomeFilters = () => {
           ? ' bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500' 
           : 'bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300'
         }`}
+        onClick={() => handleTypeClick(item.value)}
         >
           {item.name}
         </Button>
